2fb8131d3572e2fb4df4aa50b2724ffa5beaccaf
Nuño López Ansótegui <nunyo@masmedios.com>
2012-08-07 13:10:05 +0200

    Base de datos espanyol II

--- installer/data/mysql/es-ES/optional/sample_holidays.sql
+++
@@ -1,5 +0,0 @@
-
-/*Data for the table `repeatable_holidays` */
-
-insert  into `repeatable_holidays`(`id`,`branchcode`,`weekday`,`day`,`month`,`title`,`description`) values (2,'',0,0,0,'','Domingos'),(3,'',0,1,1,'','D&iacute;a de A&ntilde;o Nuevo'),(4,'',0,25,12,'','Navidad');
-
