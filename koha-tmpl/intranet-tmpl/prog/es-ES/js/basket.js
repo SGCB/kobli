0eb810133a0d7fd207c45bc38cb7e62175149b4a
Nuño López Ansótegui <nunyo@masmedios.com>
2013-05-03 15:12:17 +0200

    Traduccion Espanyol 20130503

--- koha-tmpl/intranet-tmpl/prog/es-ES/js/basket.js
+++ koha-tmpl/intranet-tmpl/prog/es-ES/js/basket.js
@@ -378,10 +378,10 @@ function showLess() {
 function updateBasket(updated_value,target) {
 	if(target){
 	target.$('#basketcount').html(" <span>("+updated_value+")</span>");
-	target.$('#cartDetails').html(_("Tu cesta contiene ")+updated_value+_(" items"));
+    target.$('#cartDetails').html(MSG_IN_YOUR_CART+updated_value);
 	} else {
 	$('#basketcount').html(" <span>("+updated_value+")</span>");
-	$('#cartDetails').html(_("Tu cesta contiene ")+updated_value+_(" items"));
+    $('#cartDetails').html(MSG_IN_YOUR_CART+updated_value);
 	}
 	var basketcount = updated_value;
 }
