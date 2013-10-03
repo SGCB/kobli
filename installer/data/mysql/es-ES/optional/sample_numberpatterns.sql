296ec34e493200ac9faf37acf743cfa690bfded9
Nuño López Ansótegui <nunyo@masmedios.com>
2013-10-03 17:51:03 +0200

    Traduccion a espanyol de nueva interfaz para subscripciones

--- installer/data/mysql/es-ES/optional/sample_numberpatterns.sql
+++ installer/data/mysql/es-ES/optional/sample_numberpatterns.sql
@@ -4,22 +4,22 @@ INSERT INTO subscription_numberpatterns
     label2, add2, every2, whenmorethan2, setto2, numbering2,
     label3, add3, every3, whenmorethan3, setto3, numbering3)
 VALUES
-    ('Number', 1, 'Simple Numbering method', 'No.{X}',
-    'Number', 1, 1, 99999, 1, NULL,
+    ('Número', 1, 'Método simple de numeración', 'No.{X}',
+    'Número', 1, 1, 99999, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL),
 
-    ('Volume, Number, Issue', 2, 'Volume Number Issue 1', 'Vol.{X}, Number {Y}, Issue {Z}',
-    'Volume', 1, 48, 99999, 1, NULL,
-    'Number', 1, 4, 12, 1, NULL,
-    'Issue', 1, 1, 4, 1, NULL),
+    ('Volumen, Número, Ejemplar', 2, 'Volumen Número Ejemplar 1', 'Vol.{X}, Número {Y}, Ejemplar {Z}',
+    'Volumen', 1, 48, 99999, 1, NULL,
+    'Número', 1, 4, 12, 1, NULL,
+    'Ejemplar', 1, 1, 4, 1, NULL),
 
-    ('Volume, Number', 3, 'Volume Number 1', 'Vol {X}, No {Y}',
-    'Volume', 1, 12, 99999, 1, NULL,
-    'Number', 1, 1, 12, 1, NULL,
+    ('Volumen, Número', 3, 'Volumen Número 1', 'Vol {X}, No {Y}',
+    'Volumen', 1, 12, 99999, 1, NULL,
+    'Número', 1, 1, 12, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL),
 
-    ('Seasonal', 4, 'Season Year', '{X} {Y}',
-    'Season', 1, 1, 3, 0, 'season',
-    'Year', 1, 4, 99999, 1, NULL,
+    ('Estacional', 4, 'Estación del año', '{X} {Y}',
+    'Estación', 1, 1, 3, 0, 'estación',
+    'Año', 1, 4, 99999, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL);
