Koha-Community/Koha

Bug 7688: Change subscription numbering pattern and frequencies
Serials numbering pattern and frequencies are no more hard-coded. Now
it's possible to create, edit and delete numbering patterns (and
frequencies). This implies new sql tables (subscription_numberpatterns
and subscription_frequencies)

Numbering patterns behave almost as before, there are still the same
values to configure (addX, everyX, settoX, whenmorethanX). lastvalueX
and innerloopX remain in subscription tables.

There is a new value in numbering patterns: numberingX. For each
"column" (X, Y or Z) you can tell how to format the number. Actually
numberingX can be set to:
 - 'dayname' (name of the day) (0-6 or 1-7 depending on which day is the
   first of the week)
 - 'monthname' (name of the month) (0-11)
 - 'season' (name of the season) (0-3) (0 is Spring)

These names are localized by using POSIX::setlocale and POSIX::strftime
and setting a 'locale' value to the subscription. Locale have to be
installed on the system.
Note that season names are not localized using POSIX::strftime (it can't
do this), so names are hardcoded into the code (available languages: en,
fr). This could be fixed in the future by using a Perl localization
framework.

Frequencies can be configured using 3 parameters:
 - 'unit': one of 'day', 'week', 'month', 'year'
 - 'issuesperunit': integer >= 1, the number of received issues per
   'unit'
 - 'unitsperissue': integer >= 1, the number of 'unit' between two
   issues
One of 'issuesperunit' and 'unitsperissue' must be equal to 1.
Examples:
  unit = 'day', issuesperunit=3, unitsperissue=1 => 3 issues per day
  unit = 'week', issuesperunit=1, unitsperissue=3 => 1 issue each 3
  weeks

Prediction pattern is now computed server-side and is more consistent
with what Koha will do. The publication date is displayed alongside the
serial number.
Irregularities can now be checked one by one, in the prediction pattern
table, or if frequency is 'day-based' (unit is 'day'), there is the
p

C4/Serials/Numberpattern.pm
@@ -1,0 +1,294 @@
+package C4::Serials::Numberpattern;
+
+# Copyright 2000-2002 Biblibre SARL
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+
+use C4::Context;
+
+use vars qw($VERSION @ISA @EXPORT);
+
+BEGIN {
+
+    # set the version for version checking
+    $VERSION = 3.01;
+    require Exporter;
+    @ISA    = qw(Exporter);
+    @EXPORT = qw(
+        &GetSubscriptionNumberpatterns
+        &GetSubscriptionNumberpattern
+        &GetSubscriptionNumberpatternByName
+        &AddSubscriptionNumberpattern
+        &ModSubscriptionNumberpattern
+        &DelSubscriptionNumberpattern
+
+        &GetSubscriptionsWithNumberpattern
+    );
+}
+
+=head3 GetSubscriptionNumberpatterns
+
+=over 4
+
+@results = GetSubscriptionNumberpatterns;
+this function get all subscription number patterns entered in table
+
+=back
+
+=cut
+
+sub GetSubscriptionNumberpatterns {
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        SELECT *
+        FROM subscription_numberpatterns
+        ORDER by displayorder
+    };
+    my $sth = $dbh->prepare($query);
+    $sth->execute;
+    my $results = $sth->fetchall_arrayref({});
+
+    return @$results;
+}
+
+=head3 GetSubscriptionNumberpattern
+
+=over 4
+
+$result = GetSubscriptionNumberpattern($numberpatternid);
+this function get the data of the subscription numberpatterns which id is $numberpatternid
+
+=back
+
+=cut
+
+sub GetSubscriptionNumberpattern {
+    my $numberpatternid = shift;
+    my $dbh = C4::Context->dbh;
+    my $query = qq(
+        SELECT *
+        FROM subscription_numberpatterns
+        WHERE id = ?
+    );
+    my $sth = $dbh->prepare($query);
+    $sth->execute($numberpatternid);
+
+    return $sth->fetchrow_hashref;
+}
+
+=head3 GetSubscriptionNumberpatternByName
+
+=over 4
+
+$result = GetSubscriptionNumberpatternByName($name);
+this function get the data of the subscription numberpatterns which name is $name
+
+=back
+
+=cut
+
+sub GetSubscriptionNumberpatternByName {
+    my $name = shift;
+    my $dbh = C4::Context->dbh;
+    my $query = qq(
+        SELECT *
+        FROM subscription_numberpatterns
+        WHERE label = ?
+    );
+    my $sth = $dbh->prepare($query);
+    my $rv = $sth->execute($name);
+
+    return $sth->fetchrow_hashref;
+}
+
+=head3 AddSubscriptionNumberpattern
+
+=over 4
+
+=item C<$numberpatternid> = &AddSubscriptionNumberpattern($numberpattern)
+
+Add a new numberpattern
+
+=item C<$frequency> is a hashref that contains values of the number pattern
+
+=item Only label and numberingmethod are mandatory
+
+=back
+
+=cut
+
+sub AddSubscriptionNumberpattern {
+    my $numberpattern = shift;
+
+    unless(
+      ref($numberpattern) eq 'HASH'
+      && defined $numberpattern->{'label'}
+      && $numberpattern->{'label'} ne ''
+      && defined $numberpattern->{'numberingmethod'}
+      && $numberpattern->{'numberingmethod'} ne ''
+    ) {
+        return undef;
+    }
+
+    my @keys;
+    my @values;
+    foreach (qw/ label description numberingmethod displayorder
+      label1 label2 label3 add1 add2 add3 every1 every2 every3
+      setto1 setto2 setto3 whenmorethan1 whenmorethan2 whenmorethan3
+      numbering1 numbering2 numbering3 /) {
+        if(exists $numberpattern->{$_}) {
+            push @keys, $_;
+            push @values, $numberpattern->{$_};
+        }
+    }
+
+    my $dbh = C4::Context->dbh;
+    my $query = "INSERT INTO subscription_numberpatterns";
+    $query .= '(' . join(',', @keys) . ')';
+    $query .= ' VALUES (' . ('?,' x (scalar(@keys)-1)) . '?)';
+    my $sth = $dbh->prepare($query);
+    my $rv = $sth->execute(@values);
+
+    if(defined $rv) {
+        return $dbh->last_insert_id(undef, undef, "subscription_numberpatterns", undef);
+    }
+
+    return $rv;
+}
+
+=head3 ModSubscriptionNumberpattern
+
+=over 4
+
+=item &ModSubscriptionNumberpattern($numberpattern)
+
+Modifies a numberpattern
+
+=item C<$frequency> is a hashref that contains values of the number pattern
+
+=item Only id is mandatory
+
+=back
+
+=cut
+
+sub ModSubscriptionNumberpattern {
+    my $numberpattern = shift;
+
+    unless(
+      ref($numberpattern) eq 'HASH'
+      && defined $numberpattern->{'id'}
+      && $numberpattern->{'id'} > 0
+      && (
+        (defined $numberpattern->{'label'}
+        && $numberpattern->{'label'} ne '')
+        || !defined $numberpattern->{'label'}
+      )
+      && (
+        (defined $numberpattern->{'numberingmethod'}
+        && $numberpattern->{'numberingmethod'} ne '')
+        || !defined $numberpattern->{'numberingmethod'}
+      )
+    ) {
+        return undef;
+    }
+
+    my @keys;
+    my @values;
+    foreach (qw/ label description numberingmethod displayorder
+      label1 label2 label3 add1 add2 add3 every1 every2 every3
+      setto1 setto2 setto3 whenmorethan1 whenmorethan2 whenmorethan3
+      numbering1 numbering2 numbering3 /) {
+        if(exists $numberpattern->{$_}) {
+            push @keys, $_;
+            push @values, $numberpattern->{$_};
+        }
+    }
+
+    my $dbh = C4::Context->dbh;
+    my $query = "UPDATE subscription_numberpatterns";
+    $query .= ' SET ' . join(' = ?,', @keys) . ' = ?';
+    $query .= ' WHERE id = ?';
+    my $sth = $dbh->prepare($query);
+
+    return $sth->execute(@values, $numberpattern->{'id'});
+}
+
+=head3 DelSubscriptionNumberpattern
+
+=over 4
+
+=item &DelSubscriptionNumberpattern($numberpatternid)
+
+Delete a number pattern
+
+=back
+
+=cut
+
+sub DelSubscriptionNumberpattern {
+    my $numberpatternid = shift;
+
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        DELETE FROM subscription_numberpatterns
+        WHERE id = ?
+    };
+    my $sth = $dbh->prepare($query);
+    $sth->execute($numberpatternid);
+}
+
+=head3 GetSubscriptionsWithNumberpattern
+
+    my @subs = GetSubscriptionsWithNumberpattern($numberpatternid);
+
+Returns all subscriptions that are using a particular numbering pattern
+
+=cut
+
+sub GetSubscriptionsWithNumberpattern {
+    my ($numberpatternid) = @_;
+
+    return unless $numberpatternid;
+
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        SELECT *
+        FROM subscription
+          LEFT JOIN biblio ON subscription.biblionumber = biblio.biblionumber
+        WHERE numberpattern = ?
+    };
+    my $sth = $dbh->prepare($query);
+    my @results;
+    if ($sth->execute($numberpatternid)) {
+        @results = @{ $sth->fetchall_arrayref({}) };
+    }
+    return @results;
+}
+
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Koha Developement team <info@koha.org>
+
+=cut

installer/data/mysql/fr-FR/2-Optionel/sample_frequencies.sql
@@ -1,0 +1,16 @@
+INSERT INTO subscription_frequencies
+    (description, unit, unitsperissue, issuesperunit, displayorder)
+VALUES
+    ('2/day', 'day', 1, 2, 1),
+    ('1/day', 'day', 1, 1, 2),
+    ('3/week', 'week', 1, 3, 3),
+    ('1/week', 'week', 1, 1, 4),
+    ('1/2 weeks', 'week', 2, 1, 5),
+    ('1/3 weeks', 'week', 3, 1, 6),
+    ('1/month', 'month', 1, 1, 7),
+    ('1/2 months', 'month', 2, 1, 8),
+    ('1/3 months', 'month', 3, 1, 9),
+    ('2/year', 'month', 6, 1, 10),
+    ('1/year', 'year', 1, 1, 11),
+    ('1/2 year', 'year', 2, 1, 12),
+    ('Irregular', NULL, 1, 1, 13);

SGCB/kobli

C4/Serials/Numberpattern.pm
@@ -1,0 +1,289 @@
+package C4::Serials::Numberpattern;
+
+# Copyright 2000-2002 Biblibre SARL
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+
+use C4::Context;
+
+use vars qw($VERSION @ISA @EXPORT);
+
+BEGIN {
+
+    # set the version for version checking
+    $VERSION = 3.01;
+    require Exporter;
+    @ISA    = qw(Exporter);
+    @EXPORT = qw(
+        &GetSubscriptionNumberpatterns
+        &GetSubscriptionNumberpattern
+        &GetSubscriptionNumberpatternByName
+        &AddSubscriptionNumberpattern
+        &ModSubscriptionNumberpattern
+        &DelSubscriptionNumberpattern
+
+        &GetSubscriptionsWithNumberpattern
+    );
+}
+
+
+=head1 NAME
+
+C4::Serials::Numberpattern - Serials numbering pattern module
+
+=head1 FUNCTIONS
+
+=head2 GetSubscriptionNumberpatterns
+
+@results = GetSubscriptionNumberpatterns;
+this function get all subscription number patterns entered in table
+
+=cut
+
+sub GetSubscriptionNumberpatterns {
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        SELECT *
+        FROM subscription_numberpatterns
+        ORDER by displayorder
+    };
+    my $sth = $dbh->prepare($query);
+    $sth->execute;
+    my $results = $sth->fetchall_arrayref({});
+
+    return @$results;
+}
+
+=head2 GetSubscriptionNumberpattern
+
+$result = GetSubscriptionNumberpattern($numberpatternid);
+this function get the data of the subscription numberpatterns which id is $numberpatternid
+
+=cut
+
+sub GetSubscriptionNumberpattern {
+    my $numberpatternid = shift;
+    my $dbh = C4::Context->dbh;
+    my $query = qq(
+        SELECT *
+        FROM subscription_numberpatterns
+        WHERE id = ?
+    );
+    my $sth = $dbh->prepare($query);
+    $sth->execute($numberpatternid);
+
+    return $sth->fetchrow_hashref;
+}
+
+=head2 GetSubscriptionNumberpatternByName
+
+$result = GetSubscriptionNumberpatternByName($name);
+this function get the data of the subscription numberpatterns which name is $name
+
+=cut
+
+sub GetSubscriptionNumberpatternByName {
+    my $name = shift;
+    my $dbh = C4::Context->dbh;
+    my $query = qq(
+        SELECT *
+        FROM subscription_numberpatterns
+        WHERE label = ?
+    );
+    my $sth = $dbh->prepare($query);
+    my $rv = $sth->execute($name);
+
+    return $sth->fetchrow_hashref;
+}
+
+=head2 AddSubscriptionNumberpattern
+
+=over 4
+
+=item C<$numberpatternid> = &AddSubscriptionNumberpattern($numberpattern)
+
+Add a new numberpattern
+
+=item C<$frequency> is a hashref that contains values of the number pattern
+
+=item Only label and numberingmethod are mandatory
+
+=back
+
+=cut
+
+sub AddSubscriptionNumberpattern {
+    my $numberpattern = shift;
+
+    unless(
+      ref($numberpattern) eq 'HASH'
+      && defined $numberpattern->{'label'}
+      && $numberpattern->{'label'} ne ''
+      && defined $numberpattern->{'numberingmethod'}
+      && $numberpattern->{'numberingmethod'} ne ''
+    ) {
+        return;
+    }
+
+    my @keys;
+    my @values;
+    foreach (qw/ label description numberingmethod displayorder
+      label1 label2 label3 add1 add2 add3 every1 every2 every3
+      setto1 setto2 setto3 whenmorethan1 whenmorethan2 whenmorethan3
+      numbering1 numbering2 numbering3 /) {
+        if(exists $numberpattern->{$_}) {
+            push @keys, $_;
+            push @values, $numberpattern->{$_};
+        }
+    }
+
+    my $dbh = C4::Context->dbh;
+    my $query = "INSERT INTO subscription_numberpatterns";
+    $query .= '(' . join(',', @keys) . ')';
+    $query .= ' VALUES (' . ('?,' x (scalar(@keys)-1)) . '?)';
+    my $sth = $dbh->prepare($query);
+    my $rv = $sth->execute(@values);
+
+    if(defined $rv) {
+        return $dbh->last_insert_id(undef, undef, "subscription_numberpatterns", undef);
+    }
+
+    return $rv;
+}
+
+=head2 ModSubscriptionNumberpattern
+
+=over 4
+
+=item &ModSubscriptionNumberpattern($numberpattern)
+
+Modifies a numberpattern
+
+=item C<$frequency> is a hashref that contains values of the number pattern
+
+=item Only id is mandatory
+
+=back
+
+=cut
+
+sub ModSubscriptionNumberpattern {
+    my $numberpattern = shift;
+
+    unless(
+      ref($numberpattern) eq 'HASH'
+      && defined $numberpattern->{'id'}
+      && $numberpattern->{'id'} > 0
+      && (
+        (defined $numberpattern->{'label'}
+        && $numberpattern->{'label'} ne '')
+        || !defined $numberpattern->{'label'}
+      )
+      && (
+        (defined $numberpattern->{'numberingmethod'}
+        && $numberpattern->{'numberingmethod'} ne '')
+        || !defined $numberpattern->{'numberingmethod'}
+      )
+    ) {
+        return;
+    }
+
+    my @keys;
+    my @values;
+    foreach (qw/ label description numberingmethod displayorder
+      label1 label2 label3 add1 add2 add3 every1 every2 every3
+      setto1 setto2 setto3 whenmorethan1 whenmorethan2 whenmorethan3
+      numbering1 numbering2 numbering3 /) {
+        if(exists $numberpattern->{$_}) {
+            push @keys, $_;
+            push @values, $numberpattern->{$_};
+        }
+    }
+
+    my $dbh = C4::Context->dbh;
+    my $query = "UPDATE subscription_numberpatterns";
+    $query .= ' SET ' . join(' = ?,', @keys) . ' = ?';
+    $query .= ' WHERE id = ?';
+    my $sth = $dbh->prepare($query);
+
+    return $sth->execute(@values, $numberpattern->{'id'});
+}
+
+=head2 DelSubscriptionNumberpattern
+
+=over 4
+
+=item &DelSubscriptionNumberpattern($numberpatternid)
+
+Delete a number pattern
+
+=back
+
+=cut
+
+sub DelSubscriptionNumberpattern {
+    my $numberpatternid = shift;
+
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        DELETE FROM subscription_numberpatterns
+        WHERE id = ?
+    };
+    my $sth = $dbh->prepare($query);
+    $sth->execute($numberpatternid);
+}
+
+=head2 GetSubscriptionsWithNumberpattern
+
+    my @subs = GetSubscriptionsWithNumberpattern($numberpatternid);
+
+Returns all subscriptions that are using a particular numbering pattern
+
+=cut
+
+sub GetSubscriptionsWithNumberpattern {
+    my ($numberpatternid) = @_;
+
+    return unless $numberpatternid;
+
+    my $dbh = C4::Context->dbh;
+    my $query = qq{
+        SELECT *
+        FROM subscription
+          LEFT JOIN biblio ON subscription.biblionumber = biblio.biblionumber
+        WHERE numberpattern = ?
+    };
+    my $sth = $dbh->prepare($query);
+    my @results;
+    if ($sth->execute($numberpatternid)) {
+        @results = @{ $sth->fetchall_arrayref({}) };
+    }
+    return @results;
+}
+
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Koha Developement team <info@koha.org>
+
+=cut

installer/data/mysql/fr-FR/2-Optionel/sample_frequencies.sql
@@ -1,0 +1,16 @@
+INSERT INTO subscription_frequencies
+    (description, unit, unitsperissue, issuesperunit, displayorder)
+VALUES
+    ('2/jour', 'day', 1, 2, 1),
+    ('1/jour', 'day', 1, 1, 2),
+    ('3/semaine', 'week', 1, 3, 3),
+    ('1/semaine', 'week', 1, 1, 4),
+    ('1/2 semaines', 'week', 2, 1, 5),
+    ('1/3 semaines', 'week', 3, 1, 6),
+    ('1/mois', 'month', 1, 1, 7),
+    ('1/2 mois', 'month', 2, 1, 8),
+    ('1/3 mois', 'month', 3, 1, 9),
+    ('2/an', 'month', 6, 1, 10),
+    ('1/an', 'year', 1, 1, 11),
+    ('1/2 ans', 'year', 2, 1, 12),
+    ('Irrégulier', NULL, 1, 1, 13);